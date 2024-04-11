800fed411bf9390a43d9f2e3e4bbe494f802de1f
Arif Hossain <alivearif120@gmail.com>
2024-04-11 11:00:06 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyUniversity(),);
+}
+
+class MyUniversity extends StatelessWidget {
+  const MyUniversity({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+      theme: ThemeData(
+     brightness: Brightness.light,
+        primaryColor: Colors.blue,
+      
+      ),
+      home: 
+    );
+  }
+}
+
