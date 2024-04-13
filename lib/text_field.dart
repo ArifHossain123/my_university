a34e2d318b421531b980745fcec485482821dcc0
Arif Hossain <alivearif120@gmail.com>
2024-04-13 10:26:25 +0200

    TextField

--- lib/text_field.dart
+++ lib/text_field.dart
@@ -5,30 +5,99 @@ class StudentTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(
-            bottom: 10,
-          ),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              labelText: 'Name',
-              labelStyle: TextStyle(
-                fontSize: 20,
-              ),
-              fillColor: Colors.white,
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Colors.blue,
-                  width: 2,
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(
+              bottom: 20,
+            ),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Name',
+                labelStyle: TextStyle(
+                  fontSize: 20,
+                ),
+                fillColor: Colors.white,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 2,
+                  ),
                 ),
               ),
+              onChanged: (String name) {},
             ),
-            onChanged: (String name) {},
           ),
-        ),
-      ],
+        
+         Padding(
+            padding: const EdgeInsets.only(
+              bottom: 20,
+            ),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Name',
+                labelStyle: TextStyle(
+                  fontSize: 20,
+                ),
+                fillColor: Colors.white,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 2,
+                  ),
+                ),
+              ),
+              onChanged: (String name) {},
+            ),
+          ),
+        
+         Padding(
+            padding: const EdgeInsets.only(
+              bottom: 20,
+            ),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Name',
+                labelStyle: TextStyle(
+                  fontSize: 20,
+                ),
+                fillColor: Colors.white,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 2,
+                  ),
+                ),
+              ),
+              onChanged: (String name) {},
+            ),
+          ),
+        
+         Padding(
+            padding: const EdgeInsets.only(
+              bottom: 20,
+            ),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Name',
+                labelStyle: TextStyle(
+                  fontSize: 20,
+                ),
+                fillColor: Colors.white,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 2,
+                  ),
+                ),
+              ),
+              onChanged: (String name) {},
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
